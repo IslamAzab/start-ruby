4bb0e940717fa28bf0afeb004bff87ab49ddae7a
Islam Azab <islam.3azab@gmail.com>
2016-05-04 20:35:19 +0200

    added sample for card_options

--- lib/start/customer.rb
+++ lib/start/customer.rb
@@ -2,6 +2,14 @@ module Start
   class Customer < BaseResource
     self.path = '/customers'
 
+    #card_options = {
+    #        card: {
+    #          number:  "4242424242424242",
+    #          exp_month: 1,
+    #          exp_year:  2023,
+    #          cvc: 123
+    #        }
+    #      }
     def add_card id, card_options
       handle_response Start.post("#{path}/#{id}/cards", body: card_options)
     end
