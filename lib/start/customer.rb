3b313d20c40047b34fa4e7c0c7f6876c2b602aa0
Islam Azab <islam.3azab@gmail.com>
2016-05-04 20:31:55 +0200

    enable adding new cards for customers

--- lib/start/customer.rb
+++ lib/start/customer.rb
@@ -1,5 +1,9 @@
 module Start
   class Customer < BaseResource
     self.path = '/customers'
+
+    def add_card id, card_options
+      handle_response Start.post("#{path}/#{id}/cards", body: card_options)
+    end
   end
 end
